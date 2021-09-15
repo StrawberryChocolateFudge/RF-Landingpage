b8b8b74ff9a605dda1347de97b8f422631074285
StrawberryChocolateFudge <hopeter@protonmail.com>
2021-09-15 20:46:52 +0100

    removed

--- lib/view/index.ts
+++
@@ -1,9 +0,0 @@
-import { Events, Renderer, RenderType, State } from "../types";
-
-const Render : Renderer = {}
-
-document.body.addEventListener(Events.render, (e: any) => {
-  const type: RenderType = e.detail.type;
-  const props: State = e.detail.props;
-  Render[type](props);
-});
